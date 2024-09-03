df11b3860fc2df34c6490b4fd56a73d53b8a7d2b
Carmepiola22 <carmelotolosa01@gmail.com>
2024-09-03 13:52:23 -0300

    ejemplo de Chatgpt agregado, no funciona la base de datos

--- Proyecto Final/pages/example.py
+++ Proyecto Final/pages/example.py
@@ -8,7 +8,7 @@ from langchain_openai import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import WebBaseLoader
 
-os.environ['OPENAI_API_KEY'] = 'poner api'
+os.environ['OPENAI_API_KEY'] = 'sk-Iq6e7wmCc3hVBprKc0ZQiMgYva1BT8czNkChQvYJV5T3BlbkFJGvcqW773P0AyrHUJILEAtv9giDJqy3AcAWVmw91Q4A'
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
