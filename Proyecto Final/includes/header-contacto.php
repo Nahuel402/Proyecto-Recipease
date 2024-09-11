ab7562eb8868f705d74479b2674d00ee5f29dc0a
Nahuel402 <ibarranahuel9832@gmail.com>
2024-09-11 17:55:30 -0300

    Pagina contacto creada

---
+++ Proyecto Final/includes/header-contacto.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary header-Reg">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="Index.php"><img src="https://images.vexels.com/media/users/3/235848/isolated/preview/4b62529b242dcef2dbc6719899ecdd6e-chefs-kitchen-hat.png" alt="Carrito" width="40" height="40">RecipeEase</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+    </div>
+  </div>
+</nav>
