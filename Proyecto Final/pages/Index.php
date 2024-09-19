8f7af29e768135f5195142de601d47ce1fe33bd0
Tomiroca <tomi.roca06@gmail.com>
2024-09-19 16:24:40 -0300

    Avance de Carme 18/9/24

--- Proyecto Final/pages/Index.php
+++ Proyecto Final/pages/Index.php
@@ -4,43 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/Estil.css">
+    <link rel="stylesheet" href="../assets/css/style-index.css">
     <script defer src="../assets/js/Cod.js"></script>
     <title>RecipeEase</title>
 </head>
 <body class="body">
     <div class="d-flex justify-content-center align-items-center vh-100 text-center">
-        <div class="container BodyIndex">
+        <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h1 class="BodyIndex-title">RecipeEase</h1>
+                <div class="col-6">
+                <h1>RecipeEase</h1>
                 </div>
+
                 <div class="row">
-                    <div class="col-8 BodyIndex-div text-left align-items-center">
-                        <p class="BodyIndex-div-description">
-                            Bienvenido a RecipeEase, el chatbot de cocina para facilitarte a la hora de cocinar.
-                        </p>
-                        <p class="BodyIndex-div-description2">
-                            ¡Haz clic en el botón de abajo para empezar!
-                        </p>
-                    </div>
-                    
+                <div class="col-6">
+                    <h1 class=""></h1>
+                
+
+                <div class="row">
+                    <div class="col-12">
+                    <h3>Bienvenido a RecipeEase, el chatbot de cocina para facilitarte a la hora de cocinar</h3>
+                    </div>   
                 </div>
-                <div class="button-container ">
-                    <div class="col-6 col-md-6 text-center">
-                        <a href="acceder.php">
-                            <button class=" left-btn " onclick="Login()">Ingresar</button>
-                        </a>
-                    </div>
-                    <div class="col-6 col-md-6 text-center">
-                        <a href="Registrarse.php">
-                            <button class=" right-btn " onclick="Registrar()">Registro</button>
-                        </a>
-                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+
+
+                <div class="col-6">
+                    <h1 class="">  </h1>
+                </div>
+                </div>
+                
+                
+               
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
