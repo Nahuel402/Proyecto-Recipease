b3367a20d16705255057f37c5abc14436de62b12
Tomiroca <tomi.roca06@gmail.com>
2024-09-24 17:31:17 -0300

    Fucion de codigos

--- Proyecto Final/pages/historial.php
+++ Proyecto Final/pages/historial.php
@@ -5,15 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/globales.css">
-    <link rel="stylesheet" href="../assets/css/header.css">
     <link rel="stylesheet" href="../assets/css/recetas.css">
+    <link rel="stylesheet" href="../assets/css/globales.css">
     <title>Historial de Recetas - RecipeEase</title>
   
 </head>
 <body>
-    <?php include "../includes/header-registro-login.php"; ?>
+    <?php
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "recipeease";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $imagen = "";
+    $nombre = "";
+    $query = mysqli_query($conn, "SELECT Nombre, Imagen FROM usuarios WHERE ID = " . $_SESSION["IdUsuario"]) or die(mysqli_error($conn));
+    while ($row = mysqli_fetch_array($query)) {
+        $imagen = $row["Imagen"];
+        $nombre = $row["Nombre"];
+    }
+    include "../includes/header.php"; ?>
 
     <div class="container mt-5">
+        <br>
         <h1 class="text-center mb-4">Historial de Recetas</h1>
         <table class="table table-bordered">
             <thead>
