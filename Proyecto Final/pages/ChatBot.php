2700e65e3371402f96d80c9f609944254b683f95
Tomiroca <tomi.roca06@gmail.com>
2024-09-24 14:51:53 -0300

    Cambios de Eze Agregados

--- Proyecto Final/pages/ChatBot.php
+++ Proyecto Final/pages/ChatBot.php
@@ -8,10 +8,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 $imagen = "";
 $nombre = "";
-$query = mysqli_query($conn, "SELECT Nombre, Imagen FROM usuarios WHERE ID = " . $_SESSION["IdUsuario"]) or die(mysqli_error($conn));
+$query = mysqli_query($conn, "SELECT Nombre, Imagen FROM usuarios WHERE ID = " . $_SESSION["IdUsuario"] . "") or die(mysqli_error($conn));
 while ($row = mysqli_fetch_array($query)) {
     $imagen = $row["Imagen"];
     $nombre = $row["Nombre"];
@@ -24,42 +23,38 @@ while ($row = mysqli_fetch_array($query)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/estiloschate.css">
-    <link rel="stylesheet" href="../assets/css/globales.css">
+    <link rel="stylesheet" href="../assets/css/style-Chat.css">
     <title>RecipeEase Chatbot</title>
-   
 </head>
 
 <body>
-    <?php include "../includes/header.php"; ?>
+    <?php include "../includes/header-ingresa.php"; ?>
 
-    <div class="container-fluid vh-100 p-5">
-        <div class="row">
-            <div class="col-12 "> <br> </div>
-        </div>
+    <div class="container-fluid vh-100 d-flex flex-column justify-content-between p-0">
         <div class="row w-100 h-100">
             <div class="col-12 col-md-10 col-lg-10 mx-auto p-0 d-flex flex-column">
-                <div class="chat-header p-3">
+                <div class="chat-header bg-dark text-white p-3">
                     <h4 class="m-0">RecipeEase Chatbot</h4>
                 </div>
-                <div id="chat-messages" class="flex-grow-1 p-3">
+                <div id="chat-messages" class="flex-grow-1 p-3" style="background-color: #f4f4f4; overflow-y: auto;">
                     <!-- Sección donde se mostrará la respuesta -->
                     <div id="response-text" class="response-text response-message">
                         <div class="col-12 col-md-12 col-lg-12"><p id="title"></p><br></div>
-                        <div class="row">
-                            <div class="col-6"><label id="ingredients" class="response-message"></label></div>
-                            <div class="col-6"><label id="instructions"></label></div>
+                            <div class="row">
+                                <div class="col-6"><label id="ingredients" class="response-message"></div>
+                                <div class="col-6"><label id="instructions"></label></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="chat-input">
+                
+                <div class="chat-input p-3 bg-light">
                     <input type="text" id="userPrompt" class="form-control me-3" placeholder="Escribe tu mensaje aquí..." required>
-                    <button onclick="FetchOpenAIResponse()" type="submit" class="btn">Enviar</button>
+                    <button onclick="FetchOpenAIResponse()" type="submit" class="btn btn-primary">Enviar</button>
                 </div>
             </div>
         </div>
     </div>
+    <script defer src="../assets/js/chatbot.js"></script>
 
     <script>
         // Detectar cuando se presiona "Enter" en el input de texto
@@ -71,8 +66,6 @@ while ($row = mysqli_fetch_array($query)) {
             }
         });
     </script>
-    <script src="../assets/js/chatbot.js"></script>
-    <script src="../bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
