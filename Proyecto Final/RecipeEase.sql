1f2238d2b2f445be74b2c18e514c364a86c67ebc
Tomiroca <tomi.roca@gmail.com>
2024-10-23 18:13:02 -0300

    Todo listo con base de datos ahora si

--- Proyecto Final/RecipeEase.sql
+++ Proyecto Final/RecipeEase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-10-2024 a las 23:03:36
+-- Tiempo de generación: 23-10-2024 a las 23:12:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `recipeease`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `receta favorita`
+--
+
+CREATE TABLE `receta favorita` (
+  `Id_receta` int(150) NOT NULL,
+  `Id_usuario` int(150) NOT NULL,
+  `NomReceta` text NOT NULL,
+  `Receta` varchar(500) NOT NULL,
+  `Ingredientes` varchar(300) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recetas recientes`
+--
+
+CREATE TABLE `recetas recientes` (
+  `Id` int(11) NOT NULL,
+  `Id_usuario` int(11) NOT NULL,
+  `fecha` date DEFAULT current_timestamp(),
+  `NomReceta` text NOT NULL,
+  `Receta` varchar(500) NOT NULL,
+  `Ingredientes` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -51,6 +81,18 @@ INSERT INTO `usuarios` (`ID`, `Nombre`, `Email`, `Imagen`, `Contra`, `role`) VAL
 --
 
 --
+-- Indices de la tabla `receta favorita`
+--
+ALTER TABLE `receta favorita`
+  ADD PRIMARY KEY (`Id_receta`);
+
+--
+-- Indices de la tabla `recetas recientes`
+--
+ALTER TABLE `recetas recientes`
+  ADD PRIMARY KEY (`Id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -59,6 +101,18 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `receta favorita`
+--
+ALTER TABLE `receta favorita`
+  MODIFY `Id_receta` int(150) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
+
+--
+-- AUTO_INCREMENT de la tabla `recetas recientes`
+--
+ALTER TABLE `recetas recientes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
